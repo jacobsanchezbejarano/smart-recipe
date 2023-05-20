ce6d0cee03d7d50d3c71079028a96c10327dc416
jacobsanchezbejarano <jake_76847@hotmail.com>
2023-05-19 23:25:20 -0400

    Changes

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -16,8 +16,8 @@ const getAll_ingredients = async (req, res, next) => {
 
 const getSingle_ingredients = async (req, res, next) => {
     try {
-      const userId = new ObjectId(req.params.id);
-      const result = await mongodb.getCluster().db().collection('Ingredients').find({ _id: userId });
+      const ingredientId = new ObjectId(req.params.id);
+      const result = await mongodb.getCluster().db().collection('Ingredients').find({ _id: ingredientId });
       result.toArray().then((lists) => {
         res.setHeader('Content-Type', 'application/json');
         res.statusCode = 200;
@@ -28,17 +28,27 @@ const getSingle_ingredients = async (req, res, next) => {
     }
 };
 
+const getSingle_ingredients_by_name = async (req, res, next) => {
+  try {
+    const ingredientName = req.params.ingredient_name;
+    const result = await mongodb.getCluster().db().collection('Ingredients').find({ ingredient_name: ingredientName });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+      res.json(lists[0]);
+    });
+  }catch(error){
+    res.status(400).json({message: error});
+  }
+};
+
 const post_ingredients = async (req, res, next) => {
   console.log(req.body);
   //const data = req.body;
-  const timestamp = new Date().toJSON();
   const data = {
-        "cod_tra": req.body.cod_tra,
-        "cod_account": req.body.cod_account,
-        "date": timestamp,
-        "debe": req.body.debe,
-        "haber": req.body.haber,
-        "status": req.body.status
+        "ingredient_name": req.body.ingredient_name,
+        "category": req.body.category,
+        "measure_type": req.body.measure_type
   };
 
     const response = await mongodb.getCluster().db().collection('Ingredients').insertOne(data);
@@ -50,18 +60,14 @@ const post_ingredients = async (req, res, next) => {
 }
 
 const update_ingredients = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const timestamp = new Date().toJSON();
+  const ingredientId = new ObjectId(req.params.id);
   const data = {
-        "cod_tra": req.body.cod_tra,
-        "cod_account": req.body.cod_account,
-        "date": timestamp,
-        "debe": req.body.debe,
-        "haber": req.body.haber,
-        "status": req.body.status
+    "ingredient_name": req.body.ingredient_name,
+    "category": req.body.category,
+    "measure_type": req.body.measure_type
   };
   const response = await mongodb.getCluster().db().collection('Ingredients')
-  .replaceOne({ _id: userId }, data);
+  .replaceOne({ _id: ingredientId }, data);
   if(response.modifiedCount > 0) {
     res.status(204).send();
   } else {
@@ -70,8 +76,8 @@ const update_ingredients = async (req, res, next) => {
 }
 
 const delete_ingredients = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getCluster().db().collection('Ingredients').deleteOne({ _id: userId });
+  const ingredientId = new ObjectId(req.params.id);
+  const response = await mongodb.getCluster().db().collection('Ingredients').deleteOne({ _id: ingredientId });
   if (response.deletedCount > 0) {
     res.status(200).send();
   } else {
@@ -82,6 +88,7 @@ const delete_ingredients = async (req, res, next) => {
 module.exports = {
     getAll_ingredients,
     getSingle_ingredients,
+    getSingle_ingredients_by_name,
     post_ingredients,
     update_ingredients,
     delete_ingredients
--- routes/ingredients.js
+++ routes/ingredients.js
@@ -5,6 +5,7 @@ const { isAuthenticated } = require("../helpers/authenticate");
 
 routes.get('/', isAuthenticated, ingredientsFunctions.getAll_ingredients);
 routes.get('/:id', isAuthenticated, ingredientsFunctions.getSingle_ingredients);
+routes.get('/:ingredient_name', isAuthenticated, ingredientsFunctions.getSingle_ingredients_by_name);
 routes.post('/', isAuthenticated, ingredientsFunctions.post_ingredients);
 routes.put('/:id', isAuthenticated, ingredientsFunctions.update_ingredients);
 routes.delete('/:id', isAuthenticated, ingredientsFunctions.delete_ingredients);
